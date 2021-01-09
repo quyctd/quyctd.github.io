0b1d764e68e725ff7f66cb74a2c5e80cc0aa1a38
Quý Đinh <quy.dc98@gmail.com>
2021-01-09 16:14:06 +0700

    Initial homepage style

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,7 @@
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 const Homepage = () => {
-    return (<div><h1>This is homepage</h1></div>);
+    return (<div className={styles.container}><h1>Hi there 👋<br/>I’m Dinh Quy</h1></div>);
 };
 
 export default Homepage;
