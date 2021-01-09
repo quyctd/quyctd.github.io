6630be6106ae20f0cae02848ad2f1f8148a1c3da
Quý Đinh <quy.dc98@gmail.com>
2021-01-09 18:40:47 +0700

    Added ESLint

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import Homepage from './components/homepage';
-
-function App() {
-  return (<Homepage />);
-}
-
-export default App;
--- src/components/homepage.js
+++
@@ -1,7 +0,0 @@
-import styles from './index.module.scss';
-
-const Homepage = () => {
-    return (<div className={styles.container}><h1>Hi there 👋<br/>I’m Dinh Quy</h1></div>);
-};
-
-export default Homepage;
