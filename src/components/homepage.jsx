a2c8bfdd97a357192036efda36246d6ea68a062a
Quý Đinh <quy.dc98@gmail.com>
2021-01-10 16:40:15 +0700

    Update content

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -4,11 +4,19 @@ import styles from "./index.module.scss";
 const Homepage = () => {
   return (
     <div className={styles.container}>
-      <h1>
-        Hi there 👋
-        <br />
-        I’m Dinh Quy
-      </h1>
+      <div className={styles.main}>
+        <h1>
+          Hi there 👋
+          <br />
+          I’m Dinh Quy
+        </h1>
+        <p>I am a software engineer from Vietnam 🇻🇳</p>
+        <p>
+          I am interested in developing a wide range of skills in diverse fields
+          that would complement each other in unexpected ways.
+        </p>
+      </div>
+      <footer>Built and Designed with ❤️</footer>
     </div>
   );
 };
