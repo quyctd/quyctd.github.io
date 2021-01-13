dac0ff15ec480623718e3a0f40794a07883a20f5
Quý Đinh <quy.dc98@gmail.com>
2021-01-13 23:06:30 +0700

    Just add social links, styles later

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ParticlesBg from "particles-bg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./index.module.scss";
 
@@ -13,6 +14,29 @@ const Homepage = () => {
             <br />
             I’m Dinh Quy
           </h1>
+          <div className={styles.socialIcons}>
+            <a
+              href="https://github.com/quyctd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "github"]} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/andrewdinh98"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            </a>
+            <a
+              href="https://www.facebook.com/akashi.211"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+            </a>
+          </div>
           <p>I am a software engineer from Vietnam 🇻🇳</p>
           <p>
             I am interested in developing a wide range of skills in diverse
