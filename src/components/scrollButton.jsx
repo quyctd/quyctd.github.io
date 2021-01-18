8ad363d84a6816776e4c6d221a0102b064eb30ad
Quý Đinh <quy.dc98@gmail.com>
2021-01-18 22:32:43 +0700

    Add scroll button

---
+++ src/components/scrollButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ScrollButton = () => {
+  return (
+    <span className="scroll-btn">
+      <p>
+        <span className="mouse">
+          <span />
+        </span>
+      </p>
+    </span>
+  );
+};
+
+export default ScrollButton;
