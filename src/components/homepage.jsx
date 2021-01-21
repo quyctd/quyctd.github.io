f39394532cef55b293f2af8347fa78bed08b503c
Quý Đinh <quy.dc98@gmail.com>
2021-01-22 06:41:11 +0700

    Remove old bg

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ParticlesBg from "particles-bg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import ScrollButton from "./scrollButton";
@@ -7,51 +6,44 @@ import styles from "./index.module.scss";
 
 const Homepage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.main}>
-          <img src="/my.jpeg" alt="quyctd" className={styles.avatar} />
-          <h1 className={styles.heading}>
-            Hi there 👋
-            <br />
-            I’m Dinh Cong Quy
-          </h1>
-          <div className={styles.socialIcons}>
-            <a
-              href="https://github.com/quyctd"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "github"]} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/andrewdinh98"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "linkedin"]} />
-            </a>
-            <a
-              href="https://www.facebook.com/akashi.211"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "facebook-square"]} />
-            </a>
-          </div>
-          <p>I am a software engineer from Vietnam 🇻🇳</p>
-          <ScrollButton />
-          {/* <p>
+    <div className={styles.appHoly}>
+      <div className={styles.main}>
+        <img src="/my.jpeg" alt="quyctd" className={styles.avatar} />
+        <h1 className={styles.heading}>
+          Hi there 👋
+          <br />
+          I’m Dinh Cong Quy
+        </h1>
+        <div className={styles.socialIcons}>
+          <a href="https://github.com/quyctd" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/andrewdinh98"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+          <a
+            href="https://www.facebook.com/akashi.211"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+          </a>
+        </div>
+        <p>I am a software engineer from Vietnam 🇻🇳</p>
+        <ScrollButton />
+        {/* <p>
             I am interested in developing a wide range of skills in diverse
             fields that would complement each other in unexpected ways. So I
             engrained this idea to my lifestyle, and created a goal of learning
             one new thing in depth every trimester.
           </p> */}
-        </div>
-        {/* <footer>Built and Designed with ❤️</footer> */}
       </div>
-      <ParticlesBg type="cobweb" color="#34b4eb" num={150} bg />
-    </>
+      {/* <footer>Built and Designed with ❤️</footer> */}
+    </div>
   );
 };
 
