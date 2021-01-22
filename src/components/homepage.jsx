26d8cda496879c37ce621459a6260372064d6717
Quý Đinh <quy.dc98@gmail.com>
2021-01-22 07:17:02 +0700

    A lot of changes

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,48 +1,85 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import ScrollButton from "./scrollButton";
 import styles from "./index.module.scss";
 
 const Homepage = () => {
   return (
     <div className={styles.appHoly}>
-      <div className={styles.main}>
-        <img src="/my.jpeg" alt="quyctd" className={styles.avatar} />
-        <h1 className={styles.heading}>
-          Hi there 👋
-          <br />
-          I’m Dinh Cong Quy
-        </h1>
-        <div className={styles.socialIcons}>
-          <a href="https://github.com/quyctd" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/andrewdinh98"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={["fab", "linkedin"]} />
-          </a>
-          <a
-            href="https://www.facebook.com/akashi.211"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={["fab", "facebook-square"]} />
-          </a>
+      <div className={styles.appMain}>
+        <div className={styles.layoutContainer}>
+          <div
+            className={styles.layoutContainerStrip}
+            style={{ gridColumnStart: 1, transform: "translateX(-8px)" }}
+          />
+          <div
+            className={styles.layoutContainerStrip}
+            style={{ gridColumnStart: 4, transform: "translateX(-8px)" }}
+          />
+          <div
+            className={styles.layoutContainerStrip}
+            style={{ gridColumnStart: 7, transform: "translateX(-8px)" }}
+          />
+          <div
+            className={styles.layoutContainerStrip}
+            style={{ gridColumnStart: 10, transform: "translateX(-8px)" }}
+          />
+          <div
+            className={styles.layoutContainerStrip}
+            style={{ gridColumnStart: 13, transform: "translateX(-8px)" }}
+          />
+          <div className={styles.homeHero}>
+            <div>
+              <div className={styles.heroImg}>
+                <img src="/my.jpeg" alt="quyctd" />
+                <div />
+              </div>
+              <div className={styles.heroHeading}>
+                <h1 className={styles.typo}>
+                  Hi there 👋
+                  <br />
+                  I’m Dinh Cong Quy
+                </h1>
+                <h1 className={styles.socialIcons}>
+                  <a
+                    href="https://github.com/quyctd"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={["fab", "github"]} />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/andrewdinh98"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                  </a>
+                  <a
+                    href="https://www.facebook.com/akashi.211"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+                  </a>
+                </h1>
+              </div>
+
+              <h2 className={styles.subHeading}>
+                I am a&nbsp; 
+                <span className={styles.highLight}>software engineer</span>
+                &nbsp;from Vietnam 🇻🇳 
+                <br />
+                I am
+                interested in developing
+                a wide range of skills in diverse fields
+                that would complement each other in unexpected ways.
+              </h2>
+            </div>
+          </div>
         </div>
-        <p>I am a software engineer from Vietnam 🇻🇳</p>
-        <ScrollButton />
-        {/* <p>
-            I am interested in developing a wide range of skills in diverse
-            fields that would complement each other in unexpected ways. So I
-            engrained this idea to my lifestyle, and created a goal of learning
-            one new thing in depth every trimester.
-          </p> */}
       </div>
-      {/* <footer>Built and Designed with ❤️</footer> */}
     </div>
   );
 };
